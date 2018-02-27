e469b9ae591e878780bc2a437ba90daeb1b45ccd
unknown <ethay012@gmail.com>
2018-02-27 11:49:36 +0200

    Changed listen and send to work more like a socket, Still needs to be continued

--- udp-client-test.py
+++ udp-client-test.py
@@ -1,22 +1,40 @@
 # -*- coding: utf-8 -*-
 from tcp_packet import TCP
+import sys
 
 
 def main():
     client = TCP()
     client.connect()
     print client
-    client.send("hello me")
+    # client.send("hello me")
     # while (to_send != exit)
     #     to_send=raw_input("enter text-->")
     #     client.send(to_send)
     #     print client.recv()
     # sys.exit()
-    with open(r"SentPicture.jpg", 'rb') as my_file:
-        my_file = my_file.read()
-        client.send(my_file)
-    client.close()
-    print "Goodbye..."
+    # with open(r"SentPicture.jpg", 'rb') as my_file:
+    #     my_file = my_file.read()
+    #     client.send(my_file)
+    # client.close()
+    # print "Goodbye..."
+    name = raw_input("Type your name here --> ").capitalize()
+    while True:
+        to_send = name + ": " + raw_input("Type to your friend --> ")
+        client.send(to_send)
+        print "Waiting to receive message from your friend"
+        answer = client.recv()
+        if answer.split(' ')[1].upper() == "EXIT":
+            break
+        else:
+            print answer
+    if to_send.split(' ')[1].upper() == "EXIT":
+        client.send(to_send)
+        client.close()
+        print "Closed"
+    print "Finished"
+    sys.exit()
+
 
 
 if __name__ == '__main__':
--- udp-server-test.py
+++ udp-server-test.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from tcp_packet import TCP
+import sys
+import time
 
+WAIT_FOR_CLOSING = 5
 
 def main():
     server = TCP()
@@ -10,16 +13,33 @@ def main():
             #     print server.recv()
             #     server.send(raw_input("enter massage-->"))
             # sys.exit()
-    print server
-    print server.recv()
-    my_file = server.recv()
-    with open(r"ReceivedPicture.jpg", 'wb') as my_picture:
-        my_picture.write(my_file)
-        # if server.status == 0:
-        #     print "Server Disconnected"
-        #     break
-    print server.recv()
-    print "Goodbye..."
+    # print server
+    # print server.recv()
+    # my_file = server.recv()
+    # with open(r"ReceivedPicture.jpg", 'wb') as my_picture:
+    #     my_picture.write(my_file)
+    #     # if server.status == 0:
+    #     #     print "Server Disconnected"
+    #     #     break
+    # print server.recv()
+    # print "Goodbye..."
+    name = raw_input("Type your name here --> ").capitalize()
+    to_send = "NAME: MESSAGE"
+    while server.status and to_send.split(' ')[1].upper() != "EXIT":
+        print "Waiting to receive message from your friend"
+        answer = server.recv()
+        if answer.split(' ')[1].upper() == "EXIT":
+            break
+        else:
+            print answer
+        to_send = name + ": " + raw_input("Type to your friend --> ")
+        server.send(to_send)
+    if to_send.split(' ')[1].upper() == "EXIT":
+        server.close()
+        print "Closed"
+    print "Finished"
+    sys.exit()
+
 
 
 

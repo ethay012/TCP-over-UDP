ee9b8d622484c1c9e38ac54fa0834c11f8a793f7
Ethay Tam <ethay012@gmail.com>
2018-03-17 00:14:53 +0200

    added power point presentation and kivy installation bat file.

---
+++ Install kivy Windows.bat
@@ -0,0 +1,4 @@
+python -m pip install --upgrade pip wheel setuptools
+python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+python -m pip install kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/
+python -m pip install kivy
